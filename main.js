a92ea6c91a69d06a339e4beee35100665654b1f4
staniszmatt <matthewpstaniszewski@gmail.com>
2019-10-27 20:18:42 -0600

    Start of the setup and structure of electron. Added the colors and bcrypt password to add api's to the main node.js.

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 // Modules
 const {app, BrowserWindow} = require('electron')
+const colors = require('colors'); //Setup like react when adding processes or functions to others only this is backend api's. 
+const bcrypt = require('bcrypt');
 
+bcrypt.hash("myPlaintextPassword", 10, function(err, hash) {
+  // Store hash in your password DB.
+  console.log(hash);
+  console.log("Errors ", err);
+});
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
+console.log(colors.rainbow("This is the colors process addition for backend."))
 // Create a new BrowserWindow when `app` is ready
 function createWindow () {
 
   mainWindow = new BrowserWindow({
-    width: 1000, height: 800,
+    width: 1600, height: 800,
     webPreferences: { nodeIntegration: true }
   })
 
