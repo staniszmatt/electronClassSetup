19f40f2cbeb1fed4518f66f3d21583e75d45b091
staniszmatt <matthewpstaniszewski@gmail.com>
2019-10-30 13:00:30 -0600

    Used the webContent new window to display the web content in a new window instead in the main window. Change the image as target link instead of a image.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //Web Content property is in itself a separate module. 
 //Web Content represents the content that is loaded in the window
 // Modules
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, webContents} = require('electron') //Required to add in the webContents from electron
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -25,7 +25,21 @@ function createWindow () {
   //create a reference to the web content property
   let wc = mainWindow.webContents // Basicity short handing the method call
 
-  console.log(wc);
+  // console.log(wc); //The main windows web content
+  // console.log(webContents.getAllWebContents()) //This sends the same info in array form
+
+  //Showing the difference between doc full loaded and dom ready
+  //This also helps at the exact points content is loaded.
+  // wc.on('did-finish-load', ()=>{
+  //   console.log("Content fully loaded");
+  // })
+  // wc.on('dom-ready', ()=>{ // This should happen first since the DOM hasn't fully loaded yet.  
+  //   console.log("DOM Ready!");  
+  // })
+
+  wc.on('new-window', (e, url)=>{
+    console.log(`Creating new window for : ${url}`);
+  })
 
   // Listen for window being closed
   mainWindow.on('closed',  () => {
