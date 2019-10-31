431d1c63ba91261c0927c8f1246f4996913ec7cf
staniszmatt <matthewpstaniszewski@gmail.com>
2019-10-31 08:51:14 -0600

    Created a link to show defautl session is shared between all windows. Logged out and compared each window session to show they all refference the same object.

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 // Modules
-const {app, BrowserWindow} = require('electron')
+// Adding session from electron allows access to the main default session
+const {app, BrowserWindow, session} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow, secondWindow
 
 // Create a new BrowserWindow when `app` is ready
 function createWindow () {
@@ -12,15 +13,30 @@ function createWindow () {
     width: 1000, height: 800,
     webPreferences: { nodeIntegration: true }
   })
+  secondWindow = new BrowserWindow({
+    width: 800, height: 600,
+    x: 100, y: 100, 
+    webPreferences: { nodeIntegration: true }
+  })
+
+  //Pull session info
+  let ses = mainWindow.webContents.session
+  let ses2 = secondWindow.webContents.session
+  let defses = session.defaultSession
+  console.log('Is both sessions the same? : ',Object.is(ses, defses)) //Object.is() to check if two variables reference the same object
+  // console.log('session data: ', ses); // Gives us an object {}   
 
   // Load index.html into the new BrowserWindow
-  mainWindow.loadFile('index.html')
-
+  mainWindow.loadFile('index.html');
+  secondWindow.loadFile('index.html');
   // Open DevTools - Remove for PRODUCTION!
   mainWindow.webContents.openDevTools()
 
   // Listen for window being closed
   mainWindow.on('closed',  () => {
+    mainWindow = null
+  })
+  secondWindow.on('closed',  () => {
     mainWindow = null
   })
 }
