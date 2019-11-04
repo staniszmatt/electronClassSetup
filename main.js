74d7314be0a816123371a49c5e69d209ec063f13
staniszmatt <matthewpstaniszewski@gmail.com>
2019-11-04 10:16:55 -0700

    Added a custom session to the second window and compared the two sessions. Added to open the dev tools for the second window.

--- main.js
+++ main.js
@@ -6,8 +6,12 @@ const {app, BrowserWindow, session} = require('electron')
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow, secondWindow
 
+
 // Create a new BrowserWindow when `app` is ready
 function createWindow () {
+
+  //Create a custom session, allows this data do be synced with multiple devices for one good use!
+  let custSession = session.fromPartition('part1')
 
   mainWindow = new BrowserWindow({
     width: 1000, height: 800,
@@ -16,21 +20,30 @@ function createWindow () {
   secondWindow = new BrowserWindow({
     width: 800, height: 600,
     x: 100, y: 100, 
-    webPreferences: { nodeIntegration: true }
+    webPreferences: { 
+      nodeIntegration: true,
+      session: custSession
+    }
   })
 
   //Pull session info
   let ses = mainWindow.webContents.session
   let ses2 = secondWindow.webContents.session
   let defses = session.defaultSession
-  console.log('Is both sessions the same? : ',Object.is(ses, defses)) //Object.is() to check if two variables reference the same object
-  // console.log('session data: ', ses); // Gives us an object {}   
+
+
+  console.log('Is first session and second session the same? : ',Object.is(ses2, ses)); // Gives us an object {}  
+  console.log('Is session and default session the same? : ',Object.is(ses, defses)); //Object.is() to check if two variables reference the same object
+  console.log('Is custom session and the session the same? : ',Object.is(ses, custSession)); 
+
+  // console.log('session data: ', ses);  
 
   // Load index.html into the new BrowserWindow
   mainWindow.loadFile('index.html');
   secondWindow.loadFile('index.html');
   // Open DevTools - Remove for PRODUCTION!
-  mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools();
+  secondWindow.webContents.openDevTools();
 
   // Listen for window being closed
   mainWindow.on('closed',  () => {
