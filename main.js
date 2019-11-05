c8e907a7b350d18b6f0f3054983b2729eef3fa54
staniszmatt <matthewpstaniszewski@gmail.com>
2019-11-05 12:30:25 -0700

    Added default session. Pulled web content into main window. Verify methode with ccall back that the content loaaded then used get cookies function to log out the cookies

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 // Modules
-const {app, BrowserWindow} = require('electron')
+//A whole API for working with Cookies since electron is a web based platform so 
+//cookies are used as part of the data store.
+const {app, BrowserWindow, session} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 // Create a new BrowserWindow when `app` is ready
 function createWindow () {
+  //Short hand reference for default sessions
+  let ses = session.defaultSession;
+  //Turning the getter into a function 
+  let getCookies = ()=>{
+    // The {} means all cookies
+    ses.cookies.get({}, (err, cookies)=>{
+      console.log("Error: ", err);
+      console.log("Cookies!: ", cookies);
+    })
+  }
+
+
 
   mainWindow = new BrowserWindow({
     width: 1000, height: 800,
@@ -14,10 +28,17 @@ function createWindow () {
   })
 
   // Load index.html into the new BrowserWindow
-  mainWindow.loadFile('index.html')
+  // mainWindow.loadFile('index.html')
+  
+  //Setting up a outside source, Need to wait for content 'did-finish-load'
+  mainWindow.loadURL('https://github.com')
 
   // Open DevTools - Remove for PRODUCTION!
   mainWindow.webContents.openDevTools()
+
+  mainWindow.webContents.on('did-finish-load', e => {
+    getCookies();
+  })
 
   // Listen for window being closed
   mainWindow.on('closed',  () => {
