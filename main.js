98f9f58dcba70d16a469768ed5cadc8fec98d579
staniszmatt <matthewpstaniszewski@gmail.com>
2019-11-16 13:59:26 -0700

    Added a down load feature with a link on the DOM. ADDED CONSOLOGS OF USRING download from sessions. Used downloadItem to direct the path of the download. Used downloadOn to use the update state to track and display download progress.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Modules
-const {app, BrowserWindow} = require('electron')
+//By default will bring up a prompt for location to save file
+const {app, BrowserWindow, session} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -8,9 +9,42 @@ let mainWindow
 // Create a new BrowserWindow when `app` is ready
 function createWindow () {
 
+  let ses = session.defaultSession; 
+
   mainWindow = new BrowserWindow({
     width: 1000, height: 800,
     webPreferences: { nodeIntegration: true }
+  })
+
+  ses.on('will-download', (e, downloadItem, webContents)=>{
+
+    console.log('Start of download');
+    console.log(downloadItem.getTotalBytes());
+    console.log('File Name: ', downloadItem.getFilename());
+    
+
+    let downloadName = ('File Name: ', downloadItem.getFilename());
+    let downloadSize = ('Total Bytes: ', downloadItem.getTotalBytes());
+
+    //getting desktop path to save to the desktop 
+    downloadItem.setSavePath(app.getPath('desktop') + `/${downloadName}`); 
+
+    downloadItem.on('updated', (e, state)=>{
+
+      let received = downloadItem.getReceivedBytes();
+
+      if(state === 'progressing' && received){
+        let progress = Math.round((received/downloadSize)*100);
+        console.log(progress); 
+        webContents.executeJavaScript(`window.progress.value = ${progress }`)
+      }
+
+    })
+
+
+    // console.log('Event info: ', e);
+    // console.log('Web Contents: ', webContents);
+    
   })
 
   // Load index.html into the new BrowserWindow
